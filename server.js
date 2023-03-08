5b9253e6a89cae490206521b27601734e7abbfde
Victoria McCrady <spartanginger@gmail.com>
2023-03-08 10:41:29 -0800

    feature
    GET request to /restaurants returns all restaurants with all menus and all items.

--- server.js
+++ server.js
@@ -5,6 +5,19 @@ const {Restaurant, Menu, Item} = require("./models/index")
 const {sequelize} = require("./db")
 
 //TODO: 
+app.get('/restaurants', async (request, response) => {
+    try{
+        let restaurants = await Restaurant.findAll({include: { model: Menu, include: {model: Item}}});
+        if(restaurants){
+            response.status(200).send(restaurants)
+        } else {
+            response.status(500).send("not found")
+        }
+    }catch (err){
+        console.log(err)
+        response.sendStatus(500).send("internal server error")
+    }
+})
 
 app.listen(port, () => {
     sequelize.sync()
